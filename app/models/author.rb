58a8d14124ad466a16282944d0676659f54f3948
bleavs <bleavitt@colgate.edu>
2017-09-05 16:17:15 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+  #validating presence of the author's name
+  validates :name, presence: true
+  #making sure no authors have the same name
+  validates :name, uniqueness: true
+
+  #validating that phone num is 10 digits exactly
+  validates :phone_number, length: { is: 10 }
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+
+  validates :content, length: {minimum: 250}
+
+  validates :summary, length: {maximum: 250}
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :clickbait
+
+
+
+
+  def clickbait
+    if !["Won't Believe", "Secret", "Top", "Guess"].any? {|phrase| title.to_s.include?(phrase)}
+      errors.add(:title, "Likely not clickbaity")
+    end
+  end
+
 end
